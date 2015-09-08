bc58cf03e8dec616a42427b858b294c1929793b4
szonjao <szonja.o@me.com>
2015-09-08 19:05:41 +0200

    enter functionality, draganddrop functionality

--- js/shopapp.js
+++ js/shopapp.js
@@ -1,27 +1,50 @@
+function setDraggable(obj) {
+	obj.draggable({
+		connectToSortable:".listelements",
+		revert:"invalid"
+	});
+} 
+
 function addItem(listitem) {
 	$(".listelements").prepend('<div class="rectangle"><input type="checkbox" class="checkbox"><label>'+listitem+'</label><img class="remove" src="./css/images/trash.svg"></div>');
+	$("#add").val("");
+	setDraggable($(".rectangle"));
 };
 
 $(document).ready(function() {
 	$("button").on("click", function(){
 		$("#add").focus();
 		addItem($("#add").val());
-	/*$("#add").keydown(function(event){
-    if(event.keyCode == 13){
-        $("button").trigger("click");
-     };*/
 	});
 
-	/*$(".add").keydown(function(e){
+	$("#add").on("keydown", function(e) {
 		if (e.which === 13) {
-		addItem($"#add").val());
-		else {
+			addItem($("#add").val());
+		} else {
 
 		};
-	});*/
+	});
 
- $('.listelements').on('click', '.remove', function(events) {
+	$(".listelements").sortable({
+		revert:true
+	});
+
+	setDraggable($(".rectangle"));
+
+ $('.listelements').on('click', '.remove', function(e) {
     $(this).closest('.rectangle').remove();
+  });
+
+ $('.listelements').on('click', '.checkbox', function(e) {
+    var item=$(this).closest('.rectangle');
+    if (event.target.checked) {
+    	$(".listelements").append(item);
+    	item.addClass("selected");
+    } else {
+    	$(".listelements").prepend(item);
+    	item.removeClass("selected");
+    }
+
   });
 
 });
